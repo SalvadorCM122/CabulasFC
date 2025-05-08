c5b6569db8f16a2d52281a15cec2252d6d95ad1e
SalvadorCM122 <168441912+SalvadorCM122@users.noreply.github.com>
2025-05-08 14:18:55 +0100

    Update metodos_principais_T2.m

--- metodos_principais_T2.m
+++ metodos_principais_T2.m
@@ -306,3 +306,9 @@ f'' = (T(k-1) - 2*T(k) + T(k+1)) / h^2
 
 f' = T(k+1) - T(k) / h
 f'' = (T(k-1) - 2*T(k) + T(k+1)) / h^2
+
+%% Formula das diferenças por Crank Nicolson
+
+f'' = (T(k-1,n+1) - 2*T(k,n+1) + T(k+1,n+1) + T(k-1,n) - 2*T(k,n) + T(k+1,n)) / (2*h^2) % Se for centrada
+
+
